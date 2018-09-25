e2da11e150304b2fc4292ed185794a4ec4ac00db
Karl Hedin <karl.hedin@jayway.com>
2018-09-25 23:27:31 +0200

    load assets - css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
 
+/* 
+=== style-loader and css-loader ===
+This enables you to import './style.css' into the file that depends on that styling. Now, when that module is run, a <style> tag with the stringified css will be inserted into the <head> of your html file.
+*/
+import "./style.css";
+
 function component() {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,19 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // a <style> tag with the stringified css will be inserted into the <head> of your html file.
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
